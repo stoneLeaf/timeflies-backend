dd42654bc2e38234e4b2a19bac2dde71548226e1
stoneLeaf <owi.stoneleaf@gmail.com>
2018-12-07 17:53:14 +0100

    Customizing linting rules

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
     "env": {
         "mocha": true
-      }
-};
+    },
+    "rules": {
+        // To allow expect(res).to.be.json
+        "no-unused-expressions": "off"
+    }
+}
