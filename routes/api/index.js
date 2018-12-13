ec447270f5b3e732c2708f7fbcd7e1741f59c69f
stoneLeaf <owi.stoneleaf@gmail.com>
2018-12-13 15:51:39 +0100

    Rename route modules

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 
+// TODO: should not be here
 router.get('/ping', function (req, res, next) {
   res.status(200).json({ status: 'ok' })
 })
 
-router.use(require('./auth'))
-router.use(require('./users'))
-router.use(require('./activities'))
-router.use(require('./projects'))
+router.use(require('./auth_routes'))
+router.use(require('./users_routes'))
+router.use(require('./activities_routes'))
+router.use(require('./projects_routes'))
 
 module.exports = router
