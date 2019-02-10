12230dd43f105b630daa877ff6ede1aaac047804
stoneLeaf <owi.stoneleaf@gmail.com>
2019-02-10 21:38:32 +0100

    Implement update in user controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,17 +5,34 @@ const AuthController = require('./auth_controller')
 var UsersController = exports = module.exports = {}
 
 UsersController.create = function (req, res, next) {
-  // TODO: Remapping, not very clean?
-  req.body.profile = {
-    name: req.body.name,
-    email: req.body.email
+  const allowedBody = {
+    password: req.body.password,
+    profile: req.body.profile
   }
-  new User(req.body).save().then(function (user) {
+
+  new User(allowedBody).save().then(function (user) {
     res.status(200).json({
       profile: user.profile,
       token: AuthController.generateTokenForUser(user)
     })
   }).catch((err) => { next(err) })
+}
+
+UsersController.update = function (req, res, next) {
+  // To prevent updates outside of public properties
+  const allowedBody = {
+    profile: req.body.profile
+  }
+  if (req.body.password) {
+    allowedBody.password = req.body.password
+  }
+
+  req.user.set(allowedBody).save().then(function (user) {
+    res.status(200).json({
+      profile: user.profile,
+      token: AuthController.generateTokenForUser(user)
+    })
+  }).catch((err) => { console.error(err); next(err) })
 }
 
 UsersController.getProfile = function (req, res, next) {
--- routes/api/v1/users_routes.js
+++ routes/api/v1/users_routes.js
@@ -5,5 +5,6 @@ const UsersController = require('../../../controllers/users_controller')
 
 router.post('/users', UsersController.create)
 router.get('/profile', auth.required, UsersController.getProfile)
+router.patch('/profile', auth.required, UsersController.update)
 
 module.exports = router
