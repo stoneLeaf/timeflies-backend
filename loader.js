148f731f9544c5d93fc61e582f3bfe037aa7b0a6
stoneLeaf <owi.stoneleaf@gmail.com>
2019-02-12 15:09:35 +0100

    Add info log message when server started

--- loader.js
+++ loader.js
@@ -1,8 +1,11 @@
 const config = require('./config')
+const logger = require('./config/winston')
 const { app } = require('./app')
 
 function load () {
-  app.listen(config.port)
+  app.listen(config.port, function () {
+    logger.info(`Server listening on port ${config.port}.`)
+  })
 }
 
 if (app.isReady) {
